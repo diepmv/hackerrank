42cbba5678bd23bd81bb8d7b0a89e201aaa80787
Diep <=>
2018-03-02 11:43:30 +0700

    diagonalDifference

---
+++ diagonalDifference.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import sys
+
+def diagonalDifference(a):
+    # Complete this function
+    
+    diag1 = sum([a[i][i] for i in range(len(a))])
+    diag2 = sum([a[len(a) -i - 1][i] for i in range(len(a))])
+    return abs(diag1 - diag2)
+
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    a = []
+    for a_i in range(n):
+       a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+       a.append(a_t)
+    result = diagonalDifference(a)
+    print(result)
