c168edbae1d85c844d75507cc84fa5c5fb118f8f
Diep <=>
2018-03-26 10:34:38 +0700

    add day3 conditional statistic

---
+++ 10_days_statistics/day3_conditional_probability.py
@@ -0,0 +1,11 @@
+"""
+problem:
+ A couple has two children, one of which is a boy. What is the probability that they have two boys?
+
+solution:
+
+A: both children are boy
+C: one of the children is boy
+"""
+
+P(A|C) = P(A)* P(C|A) / P(C) = 1/4 *1/ (3/4) = 1/3
